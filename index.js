45624a1aadd34f121d46df033af46a1c092c75fd
Felipe da Silva Carvalho <60491133+FelipeCarval@users.noreply.github.com>
2020-05-26 17:57:56 -0300

    Adcionando arquivos do projeto de página pessoal.
    Hoje dia 26/05/2020, entrego os arquivos do Projeto de Página Pessoal para a disciplina de Construção de Software para WEB. Semestre 1/2020.

---
+++ index.js
@@ -0,0 +1,30 @@
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", " ");
+    }
+}
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const titulo = document.getElementById('titulo');
+typeWrite(titulo);
